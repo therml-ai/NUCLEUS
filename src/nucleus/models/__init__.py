664db598ccded613c498d4cbb2f4a06e56958412
Arthur Feeney <afeeney@uci.edu>
2026-04-13 16:15:06 -0700

    fix imports unit tests for moe-dpot and poseidon

--- src/nucleus/models/__init__.py
+++ src/nucleus/models/__init__.py
@@ -4,7 +4,6 @@ from .nucleus1_moe import Nucleus1ViTMoE, Nucleus1AxialMoE, Nucleus1NeighborMoE
 from .nucleus1_vit import Nucleus1ViT, Nucleus1AxialViT, Nucleus1NeighborViT
 from .unets import ModernUnet, ClassicUnet
 from .bubbleformer_vit import BubbleformerViT, BubbleformerFilmViT
-from nucleus.baseline.poseidon import ScOT, ScOTConfig
 from nucleus.baseline.moe_dpot import MoEPOTNet as MoEPOTNetModule
 from nucleus.data.batching import CollatedBatch
 from ._api import (
@@ -14,10 +13,18 @@ from ._api import (
 )
 
 # This is just a stupid wrapper to register the model and initialize with a dictionary.
-@register_model("poseidon")
-class Poseidon(ScOT):
-    def __init__(self, *args, **kwargs):
-        super().__init__(ScOTConfig(**kwargs))
+try:
+    import transformers
+    HAS_TRANSFORMERS_DEPS = True
+except ImportError:
+    HAS_TRANSFORMERS_DEPS = False
+if HAS_TRANSFORMERS_DEPS:
+    from nucleus.baseline.poseidon import ScOT, ScOTConfig
+
+    @register_model("poseidon")
+    class Poseidon(ScOT):
+        def __init__(self, *args, **kwargs):
+            super().__init__(ScOTConfig(**kwargs))
 
 @register_model("moe_dpot")
 class MoEPOTNet(MoEPOTNetModule):
