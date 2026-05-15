ccc42cef2b39a7eccf96811ab9dda814ce09ad84
tanishs4 <tanishs4@ics.uci.edu>
2026-05-15 16:26:18 -0700

    Reverted to old inf

--- scripts/inf.py
+++ scripts/inf.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from numpy import true_divide
 import torch
 from collections import OrderedDict
 from nucleus.models import get_model
@@ -18,69 +19,62 @@ from nucleus.plot.plot_metrics import (
     plot_bubble_counts,
 )
 from nucleus.utils.set_fp32_precision import set_fp32_precision
-from lightning import LightningModule
 
 @hydra.main(version_base=None, config_path="../config", config_name="default")
 def main(cfg: DictConfig):
+    print("entering main yay", flush=True)
     set_fp32_precision()
     
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    
     model_name = cfg.model_cfg.name
-    #model_kwargs = {
-    #    "input_fields": 4,
-    #    "output_fields": 4,
-    #    "patch_size": cfg.model_cfg.params.patch_size,
-    #    "embed_dim": cfg.model_cfg.params.embed_dim,
-    #    "processor_blocks": cfg.model_cfg.params.processor_blocks,
-    #    "num_heads": cfg.model_cfg.params.num_heads,
-    #    "num_fluid_params": cfg.model_cfg.params.num_fluid_params,
-    #}
-    
-    model_kwargs = OmegaConf.to_container(cfg.model_cfg.params, resolve=True)
+    model_kwargs = {
+        "input_fields": 4,
+        "output_fields": 4,
+        "patch_size": cfg.model_cfg.params.patch_size,
+        "embed_dim": cfg.model_cfg.params.embed_dim,
+        "processor_blocks": cfg.model_cfg.params.processor_blocks,
+        "num_heads": cfg.model_cfg.params.num_heads,
+        "num_fluid_params": cfg.model_cfg.params.num_fluid_params,
+    }
     
     if cfg.model_cfg.params.get("num_experts", None) is not None:
         model_kwargs["num_experts"] = cfg.model_cfg.params.num_experts
         model_kwargs["topk"] = cfg.model_cfg.params.topk
-    
-    model_kwargs.pop("load_balance_loss_weight", None)
-    model_kwargs.pop("z_loss_weight", None)
         
+    normalizer = get_normalizer(OmegaConf.to_container(cfg.normalizer_cfg, resolve=True))
+        
+    print("getting model yay", flush=True)
     model = get_model(model_name, **model_kwargs)
-    model = model.to(device)
-    model_data = torch.load(cfg.checkpoint_path, weights_only=False, map_location=device)
+    model = model.cuda()
+    model_data = torch.load(cfg.checkpoint_path, weights_only=False)
     weight_state_dict = OrderedDict()
     for key, val in model_data["state_dict"].items():
-        print(key, val.shape)
-        if isinstance(model, LightningModule):
-            name = key
-        else:
-            name = key[6:]
+        name = key[6:]
         weight_state_dict[name] = val
     del model_data
     model.load_state_dict(weight_state_dict)
     model.eval()
     
-    normalizer = get_normalizer(OmegaConf.to_container(cfg.normalizer_cfg, resolve=True))
-    
     # Rollouts are saved in the directory containing the checkpoint
+    print("rollouts yay", flush=True)
     save_root = pathlib.Path(cfg.checkpoint_path).parent / "inference_rollouts"
     save_root.mkdir(parents=True, exist_ok=True)
     all_test_results = []
     for test_file_path in cfg.data_cfg.test_paths:
-        test_results: TestResults = run_test(cfg, model, normalizer, test_file_path, max_timesteps=300)
+        print(f'at test_file_path', flush=True)
+        test_results: TestResults = run_test(model, test_file_path, max_timesteps=300)
         all_test_results.append(test_results)
 
         save_dir = save_root / f"{test_results.case_name}"
         save_dir.mkdir(parents=True, exist_ok=True)
-        plot_rollout(
-            save_dir=save_dir,
-            rollout=test_results.preds,
-            test_results=test_results,
-            step_size=5,
-        )
+        #plot_rollout(
+        #    save_dir=save_dir,
+        #    rollout=test_results.preds,
+        #    test_results=test_results,
+        #    step_size=5,
+        #)
         
     torch.save(all_test_results, save_root / "test_results_reinit.pt")
+    print("saved rollouts yay", flush=True)
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
