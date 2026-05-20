ae2ba4e9726f4b7c7c73f3110a752ff4c156cf12
tanishs4 <tanishs4@ics.uci.edu>
2026-05-20 13:14:53 -0700

    bug fix: no quoted in yaml files, else structure in testing changed to prevent pred from becoming a tuple, model_kwargs added for neighbor_moe

--- scripts/inf.py
+++ scripts/inf.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from numpy import true_divide
 import torch
 from collections import OrderedDict
 from nucleus.models import get_model
@@ -18,49 +19,43 @@ from nucleus.plot.plot_metrics import (
     plot_bubble_counts,
 )
 from nucleus.utils.set_fp32_precision import set_fp32_precision
-from lightning import LightningModule
 
 @hydra.main(version_base=None, config_path="../config", config_name="default")
 def main(cfg: DictConfig):
     set_fp32_precision()
-    
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     model_name = cfg.model_cfg.name
-    #model_kwargs = {
-    #    "input_fields": 4,
-    #    "output_fields": 4,
-    #    "patch_size": cfg.model_cfg.params.patch_size,
-    #    "embed_dim": cfg.model_cfg.params.embed_dim,
-    #    "processor_blocks": cfg.model_cfg.params.processor_blocks,
-    #    "num_heads": cfg.model_cfg.params.num_heads,
-    #    "num_fluid_params": cfg.model_cfg.params.num_fluid_params,
-    #}
-    
-    model_kwargs = OmegaConf.to_container(cfg.model_cfg.params, resolve=True)
-    
+
+    model_kwargs = {
+        "input_fields": 4,
+        "output_fields": 4,
+        "patch_size": cfg.model_cfg.params.patch_size,
+        "embed_dim": cfg.model_cfg.params.embed_dim,
+        "processor_blocks": cfg.model_cfg.params.processor_blocks,
+        "num_heads": cfg.model_cfg.params.num_heads,
+        "num_fluid_params": cfg.model_cfg.params.num_fluid_params,
+    }
+
+    # model_kwargs = OmegaConf.to_container(cfg.model_cfg.params, resolve=True)
+
     if cfg.model_cfg.params.get("num_experts", None) is not None:
         model_kwargs["num_experts"] = cfg.model_cfg.params.num_experts
         model_kwargs["topk"] = cfg.model_cfg.params.topk
-    
-    if "load_balance_loss_weight" in cfg.model_cfg.params:
-        model_kwargs["load_balance_loss_weight"] = cfg.model_cfg.params.load_balance_loss_weight
-        
+
     model = get_model(model_name, **model_kwargs)
     model = model.to(device)
-    model_data = torch.load(cfg.checkpoint_path, weights_only=False, map_location=device)
+    model_data = torch.load(cfg.checkpoint_path, map_location=device, weights_only=False)
+        
     weight_state_dict = OrderedDict()
     for key, val in model_data["state_dict"].items():
-        print(key, val.shape)
-        if isinstance(model, LightningModule):
-            name = key
-        else:
-            name = key[6:]
+        name = key[6:]
         weight_state_dict[name] = val
     del model_data
     model.load_state_dict(weight_state_dict)
     model.eval()
-    
+
     normalizer = get_normalizer(OmegaConf.to_container(cfg.normalizer_cfg, resolve=True))
     
     # Rollouts are saved in the directory containing the checkpoint
@@ -74,14 +69,13 @@ def main(cfg: DictConfig):
         save_dir = save_root / f"{test_results.case_name}"
         save_dir.mkdir(parents=True, exist_ok=True)
         plot_rollout(
-            save_dir=save_dir,
-            rollout=test_results.preds,
-            test_results=test_results,
-            step_size=5,
+           save_dir=save_dir,
+           rollout=test_results.preds,
+           test_results=test_results,
+           step_size=5,
         )
         
     torch.save(all_test_results, save_root / "test_results_reinit.pt")
-
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
     main()
