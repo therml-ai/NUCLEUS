217d00bc1fe2e088597a8486c35ae36e06bacc88
Arthur Feeney <afeeney@uci.edu>
2026-05-24 21:08:49 -0700

    more extensive test

--- test/data/test_forecast_dataset.py
+++ test/data/test_forecast_dataset.py
@@ -7,14 +7,27 @@ import tempfile
 
 from nucleus.data.forecast_dataset import ForecastDataset
 from nucleus.data.in_mem_forecast_dataset import InMemForecastDataset
-from nucleus.data.layout import channel_dim
+from nucleus.data.layout import channel_dim, time_dim
 
 FIELDS = ["dfun", "temperature", "velx", "vely"]
+
+fluid_params = {
+    "val1": 1,
+    "val2": 2
+}
+heater_params = {
+    "hot1": 1,
+    "hot2": 2
+}
+global_params = {
+    "g1": 1,
+    "g2": 2
+}
 
 @pytest.mark.parametrize("dataset_class", [ForecastDataset, InMemForecastDataset])
 @pytest.mark.parametrize("history_time_window", [1, 2, 8, 16])
 @pytest.mark.parametrize("future_time_window", [1, 2, 8, 16])
-@pytest.mark.parametrize("layout", ["t h w c", "t c h w"])
+@pytest.mark.parametrize("layout", ["t h w c", "t c h w", "h w t c"])
 def test_in_mem_forecast_dataset(
     dataset_class,
     history_time_window,
@@ -42,6 +55,9 @@ def test_in_mem_forecast_dataset(
                 nx_block=16,
                 ny_block=16
             )
+            params.update(fluid_params)
+            params.update({"heater": heater_params})
+            params.update(global_params)
             json_params = json.dumps(params)
             handle.write(json_params)
             
@@ -53,14 +69,18 @@ def test_in_mem_forecast_dataset(
             history_time_window,
             1,
             20,
-            [],
-            [],
-            [],
+            fluid_params,
+            heater_params,
+            global_params,
             layout,
             None,
             True
         )
         
-        assert dataset[0].input.shape[0] == history_time_window
-        assert dataset[0].target.shape[0] == future_time_window
-        assert dataset[0].input.shape[channel_dim(layout)] == 4
+        for i in range(3):
+            data = dataset[i]
+            assert data.input.shape[time_dim(layout)] == history_time_window
+            assert data.target.shape[time_dim(layout)] == future_time_window
+            assert data.input.shape[channel_dim(layout)] == 4
+            assert len(data.sim_params_tensor) == len(fluid_params) + len(heater_params) + len(global_params)
+            assert data.sim_params_tensor[0] == fluid_params["val1"]
