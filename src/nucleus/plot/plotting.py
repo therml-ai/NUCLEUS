3db46a36592052995a07670ea0c50ef54a3294d4
tanishs4 <tanishs4@ics.uci.edu>
2026-05-26 03:38:47 -0700

    reverted datasets oops

--- src/nucleus/plot/plotting.py
+++ src/nucleus/plot/plotting.py
@@ -274,7 +274,7 @@ if __name__ == "__main__":
     import h5py
     import numpy as np
     from nucleus.utils.physical_metrics import vorticity
-    with h5py.File("/share/crsp/lab/amowli/share/BubbleML_2_downsampled64_bicubic/PoolBoiling-Subcooled-OP250-2D/Twall_97.hdf5", "r") as f:
+    with h5py.File("/share/crsp/lab/amowli/share/BubbleML_2/PoolBoiling-Subcooled-OP250-2D/Twall_97.hdf5", "r") as f:
         sdf = torch.from_numpy(f["dfun"][400])
         temp = torch.from_numpy(f["temperature"][400])
         velx = torch.from_numpy(f["velx"][400])
@@ -299,7 +299,7 @@ if __name__ == "__main__":
     
     del sdf, temp, velx, vely
     
-    with h5py.File("/share/crsp/lab/amowli/share/BubbleML_2_downsampled64_bicubic/PoolBoiling-Saturated-FC72-2D/Twall_104.hdf5", "r") as f:
+    with h5py.File("/share/crsp/lab/amowli/share/BubbleML_2/PoolBoiling-Saturated-FC72-2D/Twall_104.hdf5", "r") as f:
         sat_sdf = torch.from_numpy(f["dfun"][550])
         sat_temp = torch.from_numpy(f["temperature"][550])
         sat_velx = torch.from_numpy(f["velx"][550])
